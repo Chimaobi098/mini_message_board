29931c4c359e9aa517f0506cffabb66214155443
Chimaobi <ps3hdguy@gmail.com>
2024-12-01 10:10:49 +0100

    version 1.1.3

--- db/pool.js
+++ db/pool.js
@@ -2,13 +2,6 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const connectionString =
-  process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL;
-
 module.exports = new Pool({
-  connectionString: connectionString,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  connectionString: process.env.DATABASE_URL,
 });
