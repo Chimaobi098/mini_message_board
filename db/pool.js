a54a0302282d7f28b1995af667645d66651a297f
Chimaobi <ps3hdguy@gmail.com>
2024-12-01 10:57:27 +0100

    version 1.2

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
+const connectionString =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_URL // Use private endpoint on Railway
+    : process.env.DATABASE_PUBLIC_URL; // Use public endpoint locally
+
 module.exports = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
 });
